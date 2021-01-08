45e255691778dc1edbbd8ecc59dd73aa1983b089
Steven <Mahalak.steve@outlook.com>
2021-01-08 16:24:27 -0500

    99 bottles of beer

---
+++ 99bottlesOfBeer.js
@@ -0,0 +1,24 @@
+
+let numberOfBottles = 99;
+
+function beer () {
+
+    while (numberOfBottles >= 1)
+    {
+        if(numberOfBottles != 1) {
+        
+            console.log (numberOfBottles + " bottles of beer on the wall, " + numberOfBottles + " bottles of beer. Take one down and pass it around, " + (numberOfBottles -1) + " bottles of beer on the wall.");
+    }
+
+    else {
+        console.log (numberOfBottles + " bottle of beer on the wall, " + numberOfBottles + " bottle of beer. take one down and pass it around, no more bottles of beer on the wall.");
+    }
+
+    numberOfBottles--;
+
+}
+console.log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some beer, 99 bottles of beer on the wall.");
+
+}
+
+beer();
