352e3a7743227f473e2d6b7ca8f3bee7248b52e0
Steven <Mahalak.steve@outlook.com>
2021-03-14 22:14:31 -0400

    Sunday practice

--- chunkyMonkey.js
+++ chunkyMonkey.js
@@ -130,4 +130,98 @@ let countPairs = (n, sockArray) => {
   return totalPairs
 }
 
-console.log(countPairs(numOfSocks, sockDrawer));
+// console.log(countPairs(numOfSocks, sockDrawer));
+
+//another sock merchant
+
+
+let numOfLongSocks = 8;
+let sockPile = [10, 20, 10, 50, 20, 10, 30, 10];
+let sortAndCount = (n, arr) => {
+  let sorted = arr.sort((a, b) => a - b);
+  //[10,10,10,10,20,20,30,50]
+  let pairs = 0;
+
+  for (let i = 0; i < n - 1; i++) {
+    if (sorted[i] === sorted[i + 1]) {
+      pairs++;
+      i += 1;
+    }
+  }
+  return pairs;
+
+}
+
+// console.log(countPairs(numOfLongSocks, sockPile));
+
+
+
+// Given an array find the min and max values that can be calculated by summing four of the five integers. 
+// [1,3,5,7,9]
+// min is 1+3+5+7 and max is 3+5+7+9
+// returns 16 24 
+
+let specialArray = [1, 3, 5, 7, 9]
+let miniMaxSum = (arr) => {
+  let max = arr[0];
+  let min = arr[0];
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+    total += arr[i];
+  }
+
+  let maxSum = total - min;
+  let minSum = total - max;
+
+  return (minSum + " " + maxSum);
+}
+
+// console.log(miniMaxSum(specialArray));
+
+//grading students
+// If difference between grade and next multiple of 5 is less than 3, round grade up to the next multiple of five
+// If grade is less than 38, no rounding occurs
+
+let grades = [4, 73, 67, 38, 33]
+let gradingStudents = (grades) => {
+  for (let i = 0; i < grades.length; i++) {
+    if ((grades[i] >= 38) && ((((grades[i] % 5) == 3)) || ((grades[i] % 5) == 4))) {
+      grades[i] = grades[i] + (5 - (grades[i] % 5))
+    }
+  }
+  return grades;
+}
+
+// console.log(gradingStudents(grades));
+
+//compare triplets
+// player will get a point if their number is higher than other player
+// Alice's [5, 6, 7]
+// Bob's [3, 6, 10]
+// output 1  1 
+
+let Alice = [5, 6, 7];
+let Bob = [3, 6, 10];
+
+let compareTriplets = (a, b) => {
+  let aPoints = 0;
+  let bPoints = 0;
+  for (let i = 0; i < 3; i++) {
+    if (a[i] > b[i]) {
+      aPoints++;
+    }
+    if (b[i] > a[i]) {
+      bPoints++;
+    }
+  }
+  return [aPoints, bPoints]
+}
+
+console.log(compareTriplets(Alice, Bob));
